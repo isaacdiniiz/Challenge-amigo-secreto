ee3305e0f33471407b2531c17e0ac6f9b46cc3a2
isaacdiniiz <isacpdalves@gmail.com>
2025-02-03 19:31:41 -0300

    function para adicao de amigos em uma lista

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo() {
+    let amigo = document.getElementById('amigo').value;
+    if (amigo == '') {
+        alert('Por favor, insira um nome');
+    } else {
+        amigos.push(amigo);
+    }
+    document.querySelector('input').value = '';
+}
