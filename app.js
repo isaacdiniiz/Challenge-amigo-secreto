381400a7cd95c454156d197aa5c257564ae4f7df
isaacdiniiz <isacpdalves@gmail.com>
2025-02-04 10:46:08 -0300

    mostrar amigos adicionados

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ function adicionarAmigo() {
         alert('Por favor, insira um nome');
     } else {
         amigos.push(amigo);
+        atualizaAmigos();
     }
-    document.querySelector('input').value = '';
+    let campo = document.querySelector('input');
+    campo.value = '';
+}
+
+function atualizaAmigos() {
+    document.getElementById('listaAmigos').innerHTML = '';
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = amigos[i]; 
+        document.getElementById('listaAmigos').appendChild(li);   
+    } 
 }
